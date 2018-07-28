ebe55a4bf97b68912ec4eeacc81daa9fb0384fc3
Oliver Plunkett <otplunkett@gmail.com>
2018-07-28 14:50:55 -0400

    add logging

--- src/lib/config.tsx
+++ src/lib/config.tsx
@@ -1,8 +1,6 @@
-console.log(process.env)
-
 const CONFIG = {
-  PORT: process.env.PORT || 3002,
-  API_URL: process.env.API_URL || "http://localhost:3002"
+  API_URL:
+    "https://invisible-college-api.herokuapp.com" || "http://localhost:3002"
 }
 
 export default CONFIG
