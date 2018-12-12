cda99ae90ad108e782d845d7fb23fc0e8f460be5
Oliver Plunkett <otplunkett@gmail.com>
2018-12-12 08:13:09 -0500

    config

--- src/lib/config.tsx
+++ src/lib/config.tsx
@@ -3,9 +3,7 @@ dotenv.config()
 
 const CONFIG = {
   API_URL:
-    /*"https://invisible-college-api.herokuapp.com" || */ "http://localhost:3002",
-  DISCOVER_API_URL:
-    /*"https://discover9292.herokuapp.com" || */ "http://localhost:5000"
+    "https://invisible-college-api.herokuapp.com" || "http://localhost:3002"
 }
 
 export default CONFIG
